0ea0b51fd285b98888d4cfafd677fa4eea6f3704
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2016-04-11 13:08:49 +0200

    installation: pytest-runner addition
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- setup.py
+++ setup.py
@@ -25,10 +25,8 @@
 """Jinja utilities for Invenio."""
 
 import os
-import sys
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand
 
 readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
@@ -57,52 +55,20 @@ for reqs in extras_require.values():
 
 setup_requires = [
     'Babel>=1.3',
+    'pytest-runner>=2.7.0',
 ]
 
 install_requires = [
     'arrow>=0.7.0',
+    'CairoSVG>=1.0.20',
     'Flask-BabelEx>=0.9.2',
     'Flask>=0.10',
     'Jinja2>=2.8',
-    'CairoSVG>=1.0.20',
     'pillow>=3.2.0',
 ]
 
 packages = find_packages()
 
-
-class PyTest(TestCommand):
-    """PyTest Test."""
-
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        """Init pytest."""
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-        try:
-            from ConfigParser import ConfigParser
-        except ImportError:
-            from configparser import ConfigParser
-        config = ConfigParser()
-        config.read('pytest.ini')
-        self.pytest_args = config.get('pytest', 'addopts').split(' ')
-
-    def finalize_options(self):
-        """Finalize pytest."""
-        TestCommand.finalize_options(self)
-        if hasattr(self, '_test_args'):
-            self.test_suite = ''
-        else:
-            self.test_args = []
-            self.test_suite = True
-
-    def run_tests(self):
-        """Run tests."""
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 # Get the version string. Cannot be done with import!
 g = {}
@@ -152,5 +118,4 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Development Status :: 1 - Planning',
     ],
-    cmdclass={'test': PyTest},
 )
