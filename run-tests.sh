f87fb61d2c9f3ecf6471003ec10e9b6394fa428d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-05-17 11:09:39 +0200

    global: clean test infrastructure

--- run-tests.sh
+++ run-tests.sh
@@ -3,17 +3,19 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2022 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-set -o errexit 
+# Quit on errors
+set -o errexit
 
-set -o nounset 
+# Quit on unbound symbols
+set -o nounset
 
-pydocstyle invenio_formatter tests docs
-python -m check_manifest --ignore ".*-requirements.txt"
-isort invenio_formatter tests --check-only --diff
-sphinx-build -qnNW docs docs/_build/html
-python setup.py test
-sphinx-build -qnNW -b doctest docs docs/_build/doctest
+python -m check_manifest
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+python -m pytest
+tests_exit_code=$?
+exit "$tests_exit_code"
