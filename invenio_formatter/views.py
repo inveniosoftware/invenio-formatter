a53cade4ee0ad6f98f42b2638b0758688d9217f0
Fatimah Zulfiqar <51617644+0einstein0@users.noreply.github.com>
2024-04-02 11:20:37 +0200

    badge: added sanitization for value (#109)
    Co-authored-by: Fatimah <fatimah.zulfiqar@cern.ch>

--- invenio_formatter/views.py
+++ invenio_formatter/views.py
@@ -16,6 +16,8 @@ from datetime import timedelta
 
 from flask import Blueprint, Response, current_app, request
 
+from .filters.html import sanitize_html
+
 
 def create_badge_blueprint(allowed_types):
     """Create the badge blueprint.
@@ -41,6 +43,7 @@ def create_badge_blueprint(allowed_types):
     )
     def badge(title, value, ext="svg"):
         """Generate a badge response."""
+        value = sanitize_html(value)  # Added sanitization for value in badge generation
         if ext == "svg":
             generator = generate_badge_svg
             mimetype = "image/svg+xml"
