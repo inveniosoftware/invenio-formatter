967a4a38141934867a12fdd494fa468de41eed2f
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-29 23:55:54 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * bump requires-python due to dependencies requiring 3.9+
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2015-2018 CERN.
-# SPDX-FileCopyrightText: 2021 TU Wien.
-# SPDX-License-Identifier: MIT
-
-"""Jinja utilities for Invenio."""
-
-from setuptools import setup
-
-setup()
